f795da349a5bb594e3277cb2bc19332b775e9826
soywiz <soywiz@gmail.com>
2023-01-25 20:48:17 +0100

    Update to korge 4.0.0-alpha-2

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -1,35 +1,21 @@
 import com.soywiz.korge.gradle.*
 
 plugins {
-    id("com.soywiz.korge") version "3.4.0"
-	//id("com.soywiz.korge") version "999.0.0.999"
+    id("com.soywiz.korge") version "4.0.0-alpha-2"
 }
 
 korge {
-	id = "com.sample.demo"
-
-// To enable all targets at once
-
-	//targetAll()
-
-// To enable targets based on properties/environment variables
-	//targetDefault()
-
-// To selectively enable targets
+	id = "org.korge.samples.mascots"
 
 	targetJvm()
-	//targetJs()
-	//targetDesktop()
-	//targetIos()
-	//targetAndroidIndirect() // targetAndroidDirect()
-
+	targetJs()
+	targetDesktop()
+	targetIos()
+	targetAndroidDirect()
 	serializationJson()
-	//targetAndroidDirect()
 }
-
 
 dependencies {
     add("commonMainApi", project(":deps"))
-    //add("commonMainApi", project(":korge-dragonbones"))
 }
 
